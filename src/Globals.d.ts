0824b86a8901106de3a23839f99fdd8a39fc09c8
Vladislav Tikhonov <vladislav000001@gmail.com>
2022-12-15 11:21:56 +0400

    Add globals

---
+++ src/Globals.d.ts
@@ -0,0 +1,13 @@
+declare module "*.module.css";
+declare module "*.module.sass";
+
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}

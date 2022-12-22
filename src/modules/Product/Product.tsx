9c9f5544a750a81b7fd0a2f447374e1725b521d3
Vladislav Tikhonov <vladislav000001@gmail.com>
2022-12-22 16:54:40 +0400

    Add product page & Product cart component

---
+++ src/modules/Product/Product.tsx
@@ -0,0 +1,5 @@
+import styles from "./Product.module.sass";
+
+export const Product = () => {
+  return <div>Product page</div>;
+};
---
+++ src/shared-components/ProductCard/ProductCard.tsx
@@ -0,0 +1,5 @@
+import styles from "./ProductCard.module.sass";
+
+export const ProductCard = () => {
+  return <div>card</div>;
+};

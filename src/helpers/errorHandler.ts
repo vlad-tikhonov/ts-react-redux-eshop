33028b7699915e9ec8553d7ea770fe83086f1f90
Vladislav Tikhonov <vladislav000001@gmail.com>
2023-03-19 16:25:01 +0400

    Error handler fix

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -1,12 +1,11 @@
 import { AxiosError } from "axios"
 
 export const errorHandler = (e: unknown): string | string[] => {
-	console.log(e);
-					if (e instanceof AxiosError && e.response?.data.message){
-					return e.response.data.message
-				} else if (e instanceof Error) {
-					return e.message
-				} else {
-					return 'Unknown error'
-				}
+	if (e instanceof AxiosError && e.response?.data.message){
+		return e.response.data.message
+	} else if (e instanceof Error) {
+		return e.message
+	} else {
+		return 'Unknown error'
+	}
 }

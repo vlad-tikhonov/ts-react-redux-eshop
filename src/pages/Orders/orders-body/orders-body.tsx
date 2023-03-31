632df799d800f11e3e46708189650736c1722fd8
Vladislav Tikhonov <vladislav000001@gmail.com>
2023-03-31 13:19:56 +0400

    Orders adaptive

--- src/pages/Orders/orders-body/orders-body.tsx
+++ src/pages/Orders/orders-body/orders-body.tsx
@@ -15,20 +15,18 @@ export const OrdersBody = ({ orders }: OrdersBodyProps) => {
       {orders.map((order) => (
         <div className={styles.order} key={order._id}>
           <div className={styles.order_header}>
-            <div className={styles.left}>
-              <Text size="l" bold className={styles.date}>
-                {order.date}
-              </Text>
-              <Text size="l" bold className={styles.time}>
-                {order.time}
-              </Text>
-              <Notice size="m" accent="gray">
-                В процессе
-              </Notice>
-            </div>
-            <div className={styles.right}>
-              <Text size="l">{modifyPrice(order.total)}</Text>
-            </div>
+            <Text size="l" bold className={styles.date}>
+              {order.date}
+            </Text>
+            <Text size="l" bold className={styles.time}>
+              {order.time}
+            </Text>
+            <Notice size="m" accent="gray">
+              В процессе
+            </Notice>
+            <Text size="l" className={styles.total}>
+              {modifyPrice(order.total)}
+            </Text>
           </div>
           <div className={styles.order_products}>
             {order.products.map((productInfo) => (

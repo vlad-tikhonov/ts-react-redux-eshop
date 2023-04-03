c6b710bee37cf961c9948b7b03e0426bce2013de
Vladislav Tikhonov <vladislav000001@gmail.com>
2023-04-03 22:57:15 +0400

    server fix

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-import express from 'express'
-import path from 'path'
+const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
+const app = express();
 
-app.use(express.static(__dirname))
-app.use(express.static(path.resolve(__dirname, 'build')))
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, "build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-
-app.listen(PORT)
+app.listen(PORT);

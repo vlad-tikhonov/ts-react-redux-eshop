99fa47ef9f0e61c9c3cd3d5308cebd061008c94f
Vladislav Tikhonov <vladislav000001@gmail.com>
2023-04-06 17:10:36 +0400

    Htag refactoring

--- src/ui/htag/htag.tsx
+++ src/ui/htag/htag.tsx
@@ -3,25 +3,53 @@ import { ReactNode } from "react";
 import { ElementSizes } from "types/element-props";
 import styles from "./htag.module.sass";
 
-interface HtagProps {
+interface HtagProps extends React.HTMLAttributes<HTMLHeadingElement> {
   size: ElementSizes;
   children: ReactNode;
-  className?: string;
 }
 
-export const Htag = ({ size, children, className }: HtagProps) => {
+export const Htag = ({
+  size,
+  children,
+  className,
+  ...restProps
+}: HtagProps) => {
   switch (size) {
     case "xl":
-      return <h1 className={cn(styles.h1, className)}>{children}</h1>;
+      return (
+        <h1 className={cn(styles.h1, className)} {...restProps}>
+          {children}
+        </h1>
+      );
     case "l":
-      return <h2 className={cn(styles.h2, className)}>{children}</h2>;
+      return (
+        <h2 className={cn(styles.h2, className)} {...restProps}>
+          {children}
+        </h2>
+      );
     case "m":
-      return <h3 className={cn(styles.h3, className)}>{children}</h3>;
+      return (
+        <h3 className={cn(styles.h3, className)} {...restProps}>
+          {children}
+        </h3>
+      );
     case "s":
-      return <h4 className={cn(styles.h4, className)}>{children}</h4>;
+      return (
+        <h4 className={cn(styles.h4, className)} {...restProps}>
+          {children}
+        </h4>
+      );
     case "xs":
-      return <h5 className={cn(styles.h5, className)}>{children}</h5>;
+      return (
+        <h5 className={cn(styles.h5, className)} {...restProps}>
+          {children}
+        </h5>
+      );
     default:
-      return <h1 className={cn(styles.h1, className)}>{children}</h1>;
+      return (
+        <h1 className={cn(styles.h1, className)} {...restProps}>
+          {children}
+        </h1>
+      );
   }
 };
